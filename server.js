d07615221b770ce26344010c5360bc9d807f90e8
Fabiano Ramires <fdrtec@gmail.com>
2020-09-18 02:05:37 -0300

    inclusão do express

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('./dist/angular-base'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/angular-base'});
+});
+
+app.listen(process.env.PORT || 8080);
